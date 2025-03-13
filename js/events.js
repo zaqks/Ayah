9489721d8c7ec004f6dac8c44441428276aae619
zak <imzakbro@gmail.com>
2025-03-13 11:26:56 +0100

    fullscreen

--- js/events.js
+++ js/events.js
@@ -12,3 +12,30 @@ document.getElementById("chaps_ttl").onclick = function () {
 // MOVES
 document.getElementById("prev").onclick = () => change_aya(aya - 1);
 document.getElementById("next").onclick = () => change_aya(aya + 1);
+
+// FULLSCREEN
+IDS.addEventListener("click", function () {
+  const element = document.documentElement; // Target the root <html> element
+
+  if (!document.fullscreenElement) {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+      /* Safari */
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      /* IE11 */
+      element.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+});
